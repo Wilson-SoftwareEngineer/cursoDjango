60442a2769db734d53612e6d62679eb803ad8ca3
Wilson Sousa <apc.wss@gmail.com>
2025-11-23 15:21:50 -0300

    add app products

---
+++ products/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+# Create your views here.
+def product_list(request):
+    return render(request, 'products/products_list.html')                
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import homepage, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),       # http://127.0.0.1:8000/
-    path('about/', about, name='about'),       # http://127.0.0.1:8000/about/
+    path('about/', about, name='about'), 
+    path('products/', include('products.urls')),  # http://127.0.0.1:8000/products/
 ]
